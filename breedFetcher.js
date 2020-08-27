4db403ab49eb64ec54a19f91a821a4682649e2dd
DevHalpin <echidnablade@hotmail.com>
2020-08-27 20:02:06 +0000

    Refactored breedFetcher for m/c unit testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require('request');
 
-const getUserInput = () => process.argv.slice(2);
-
-const requestString = 'https://api.thecatapi.com/v1/breeds/search?q=' + getUserInput();
-
-request(requestString, (error, response, body) => {
-  if (!error && response && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    if (data[0]) {
-      console.log(data[0].description);
-    } else {
-      console.log('Are you sure that\'s a real cat?');
+const fetchBreedDescription = (breedName, callback) => {
+  const requestString = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(requestString, (error, response, body) => {
+    if (!error && response && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      if (data[0]) {
+        callback(null,data[0].description);
+      } else {
+        callback(null, 'Are you sure this is a real cat?');
+      }
+    } else if (error) {
+      callback(error, null);
     }
-  } else if (error) {
-    console.log('An error has occured: ', error);
-  } else if (response && response.statusCode) {
-    console.log('Website returned an error code: ', response.statusCode);
-  }
-    
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
